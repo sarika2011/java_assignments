a3e016ec2125926d46d6af0eed6fedb6c7dd532c
sarika2011 <114053434+sarika2011@users.noreply.github.com>
2022-09-26 13:39:13 +0530

    Add files via upload

---
+++ empdetails.java
@@ -0,0 +1,31 @@
+package testExeercise;
+
+public class empdetails{
+	
+	    	static {
+	    	String FirstName1 ="Mohit";
+	    	String LastName1 ="Singh";
+	    	int age1= 15;	
+	    	System.out.println("First name is" + "" + FirstName1);
+	        System.out.println("Last name is"  + "" + LastName1);  
+	        System.out.println("Age is" +"" + age1);  
+	    	}
+	    	
+	    	public static void main(String args[]){  	
+	    		details();	
+	    			    }  
+	    	
+	    	static void details()
+	    	{
+	    	  String FirstName="sarika";
+	    	  String LastName="Gupta";
+	    	  int age=25;
+	    	  System.out.println("First name is" + "" +FirstName);
+	          System.out.println("Last name is"  + "" +LastName);  
+	          System.out.println("Age is" +"" +age);  
+	    	}
+	    	
+	    	
+	}  
+	
+	
